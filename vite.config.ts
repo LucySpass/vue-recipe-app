325d100053cdf29c207ec2c28472475b05a55232
Ivana Begovic <ivana.begovic@yahoo.com>
2026-03-08 15:12:05 +0100

    fix github pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
-import { fileURLToPath, URL } from "node:url";
 import tailwindcss from "@tailwindcss/vite";
 import vue from "@vitejs/plugin-vue";
+import { fileURLToPath, URL } from "node:url";
 import { defineConfig } from "vite";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/',
   plugins: [vue(), tailwindcss()],
   resolve: {
     alias: {
